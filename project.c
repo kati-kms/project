901fd0dcc68213c0b80e75f81f0ffac831cae5c9
user-kang <qkrguswndk@naver.com>
2016-05-30 20:43:32 +0900

    프로젝트의 시작

---
+++ project.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#define N 59
+//	int add_function ()
+//	int minus_function ()
+//	int multi_function ()
+//	int divide_function ()
+//	int 소수점 ()
+//		.을만나면 들어오고 
+//	int 컴마 ()
+//		stack을 해서 다시 역순으로 출력해서 stack의 k가 3의배수일때 ,찍기
+//  int 우선순위 ()
+//  int 변수()
+//  int 자릿수()
+//  int _clear ()
+//  int _load()
+//  int _save()
+//  int _end()
+//  int _var()
+//  int _error()
+
+
+
+int main () {
+	char c;
+
+	while((c=getchar())!=13) //end of line
+	{
+//		if ((c = getchar())== 32)
+//			continue;
+		for(i=9 ; i<N-1 ; i++)
+		{
+			stack[]
+		}
+		
+	for(i=1 ; i<3 ; i++)		
+		while((c=getchar()!=32)
+			num(i) 배열에 넣기 	//space바가 오면 종료
+
+	}
+
+	printf("Start...\n");
+	
+}
+
+
+int add_function () {
+	char num1[N]={0};
+	char num2[N]={0};
+	char num3[N+1]={0};
+
+
+	for (i=1 ; i<=N ; i++)
+	{
+		num3[i] = num1[i]+num2[i];
+		if(num3[i]>10) // 더했을때 10보다큰경우
+		{
+			num3[i]-=10;
+			num3[i+1]+=1;
+		}
+	}
+}
+
+int 자릿수 () {
+	
